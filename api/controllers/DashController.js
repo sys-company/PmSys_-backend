3c1c8890a8f2b232cb009b337fc27160dea70b38
marcorover <marcomorover@gmail.com>
2019-11-27 19:56:46 -0300

    testes rota dash

--- api/controllers/DashController.js
+++ api/controllers/DashController.js
@@ -1,15 +1,15 @@
-const SquadModel = require('../models/DashModel');
+const DashModel = require('../models/DashModel');
 
 const getData = async (req, res) => {
     
     const {  } = req.body;
 
-    model = new Dashodel();
+    model = new DashModel();
 
     const dashData = await model.select();
 
-    // if(session.length > 0) {
-    //     return res.status(200).json(dashData);
+    //if(session.length > 0) {
+         return res.status(200).json(dashData);
     // }else {
     //     return res.status(404).end();
     // }
